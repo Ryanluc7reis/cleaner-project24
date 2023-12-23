870c2328b60493eddd6a079a9c7e9a908b83422e
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2023-12-23 02:19:11 -0300

    Criado tipografia H2 E H5

---
+++ src/components/typography/H2.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const H2 = styled.h2`
+  font-size: 55px;
+  font-weight: bold;
+  color: #fff;
+`;
+export default H2;
---
+++ src/components/typography/H5.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const H5 = styled.h5`
+  font-size: 25px;
+  font-weight: 500;
+   @media (max-width: 450px) {
+    font-size: 28px;
+  }
+
+
+`
+export default H5
