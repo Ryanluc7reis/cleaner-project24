8400e32313fe154e9c70ac45d23f16716acaf36f
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2023-12-23 01:00:02 -0300

    Criado pages e configurado eslint e pretier

---
+++ pages/_app.jsx
@@ -0,0 +1,36 @@
+import { createGlobalStyle,ThemeProvider } from 'styled-components';
+import theme from '@/src/styles/theme';
+
+const GlobalStyles = createGlobalStyle`
+
+* { 
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  //overflow-x: hidden;
+}
+html{
+ font-size: 62.5%;
+}
+body {
+  font-family: 'Open Sans', sans-serif;
+}
+h1,h2,h3,h4,h5 {
+ font-family: ${props => props.theme.font.family.heading}
+}
+ `
+;
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme} >
+      <>
+        <Component {...pageProps}  />
+        <GlobalStyles />
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
