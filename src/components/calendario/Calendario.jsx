01071efb8b7ecfabab826db1864059f043e640d4
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2023-12-23 02:13:28 -0300

    Criado calendario

---
+++ src/components/calendario/Calendario.jsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import dayjs from "dayjs";
+import format from "date-fns/format";
+import styled from "styled-components";
+
+export default function BasicDateCalendar(props) {
+	const hoje = (data) => {
+		const today = dayjs();
+		return data.isSame(today, "day");
+	};
+
+	const shouldDisableDate = (data) => hoje(data);
+
+	return (
+		<LocalizationProvider dateAdapter={AdapterDayjs} {...props}>
+			<DateCalendar
+				shouldDisableDate={shouldDisableDate}
+				disablePast
+				{...props}
+			/>
+		</LocalizationProvider>
+	);
+}
