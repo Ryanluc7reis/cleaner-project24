375691fc485e8fb58566b06568e7937390545e2b
g4b3ll <gabriel_machado85@yahoo.com.br>
2023-12-27 20:03:07 -0300

    alterando estilo do erro de input

--- src/components/sectionOne/SectionOne.jsx
+++ src/components/sectionOne/SectionOne.jsx
@@ -55,17 +55,18 @@ const StyledForm = styled.form`
 
 export const ErrorMessage = styled.span`
   position: absolute;
-  color: #fbd0d0;
-  background-color: #1a1aecb0;
+  color: #000;
+  background-color: #ffffffb7;
+  border: 1.2px solid #ff0000;
   width: 180px;
   height: 74px;
   text-align: center;
-  border: 2px solid #1a1aecc0;
+  border-radius: 10px;
   padding: 18px 36px;
   font-weight: bolder;
   font-size: 14px;
   transform: translate(10%, 70%);
-	@media (max-width: 768px){
+  @media (max-width: 768px) {
     font-size: 17px;
     padding: 10px 36px;
   }
@@ -79,10 +80,10 @@ export const ErrorMessage = styled.span`
     height: 0;
     border-left: 15px solid transparent;
     border-right: 15px solid transparent;
-    border-top: 15px solid #1a1aec9e;
+    border-top: 15px solid #ffffffdd;
   }
 
-  @media (max-width: 900px){
+  @media (max-width: 900px) {
     transform: translate(-70%, 70%);
   }
 `
@@ -99,9 +100,9 @@ export default function SectionOne() {
 
   const handleSubmit = () => {
     if (valor.trim().length < 3) {
-      setError(true);
+      setError(true)
     } else {
-      setError(false);
+      setError(false)
       router.push(`/plansScreen?region=${encodeURIComponent(valor)}`)
     }
   }
@@ -112,15 +113,17 @@ export default function SectionOne() {
         <Navbar />
         <StyledContainer1>
           <H2>Find Top Rated Cleaners!</H2>
-          <StyledH5 id='input1'>
+          <StyledH5 id="input1">
             <H5>• Change Your Cleaner at Anytime</H5>
             <H5>• Dedicated Customer Service</H5>
             <H5>• Liability Insured Up to £4M</H5>
           </StyledH5>
           {error && <ErrorMessage>That region is invalid.</ErrorMessage>}
-          <StyledForm >
-            <Input  placeholder="Enter your region" value={valor} onChange={handleInputChange} />
-            <Button type='button' onClick={handleSubmit} >Let´s go</Button>
+          <StyledForm>
+            <Input placeholder="Enter your region" value={valor} onChange={handleInputChange} />
+            <Button type="button" onClick={handleSubmit}>
+              Let´s go
+            </Button>
           </StyledForm>
         </StyledContainer1>
       </BoxShadow>
