d6c5a9d6b2a7940277d1ac7bc615167bc95cccb5
g4b3ll <gabriel_machado85@yahoo.com.br>
2024-01-02 21:22:48 -0300

    Adicionando novo ícone e tema

--- theme.js
+++ theme.js
@@ -6,14 +6,13 @@ const theme = {
     delfblu: '#454B66',
     ultravio: '#56648F',
     azulclaro: '#677DB7',
-    inputBackground : '#F5F5F5',
-    inputBorder : '#E6E6E6',
-    greenMoney: '#1eff00e6',
-    
+    inputBackground: '#F5F5F5',
+    inputBorder: '#E6E6E6',
+    greenMoney: '#1EFF00E6'
   },
   font: {
     family: {
-     heading: " 'Source Sans 3', sans-serif"
+      heading: " 'Source Sans 3', sans-serif"
     },
     size: {
       xsmall: '8rem',
@@ -23,9 +22,9 @@ const theme = {
       xlarge: '4.0rem',
       xxlarge: '4.8rem',
       huge: '5.6rem',
-      xhuge: '6.2rem',
-    },
-  }, 
+      xhuge: '6.2rem'
+    }
+  },
   spacings: {
     xsmall: '8rem',
     small: '1.6rem',
@@ -34,7 +33,7 @@ const theme = {
     xlarge: '4.0rem',
     xxlarge: '4.8rem',
     huge: '5.6rem',
-    xhuge: '6.2rem',
-  },
+    xhuge: '6.2rem'
+  }
 }
 export default theme
