608fa827086f019f69676fb2baa220bd78000e69
g4b3ll <gabriel_machado85@yahoo.com.br>
2024-01-04 16:36:16 -0300

    Adicionando tag lang no documento

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,17 +1,14 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 // Ainda cria
 class NewDocument extends Document {
   render() {
     return (
       <Html>
+        <meta lang="en-us" />
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,700;1,500&display=swap"
             rel="stylesheet"
@@ -22,7 +19,7 @@ class NewDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
-export default NewDocument;
+export default NewDocument
