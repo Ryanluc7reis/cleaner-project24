d9df2df815385638399ca8166894791941344284
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2024-01-22 18:51:42 -0300

    Alianhando commit

--- src/components/listcleaners/ListCleaners.jsx
+++ src/components/listcleaners/ListCleaners.jsx
@@ -199,8 +199,8 @@ const cleanersData = [
     cleaningCount: 100,
     experience: 'Since born'
   }
-  // Adicione mais objetos de dados conforme necessário
 ]
+export { cleanersData }
 const ButtonAltStyled = styled(Button)`
   width: 145px;
   height: 40px;
@@ -209,38 +209,35 @@ const ButtonAltStyled = styled(Button)`
   font-size: 12px;
 `
 
-const Quit = styled.p`
-  color: red;
-  font-size: 18px;
-`
-
 export default function ListCleaners(props) {
   const [showOption, setshowOption] = useState(false)
   const [selectedCleaner, setSelectedCleaner] = useState(null)
   const [updateShortby, setupdateShortby] = useState(null)
   const [selectedCardIndex, setSelectedCardIndex] = useState(null)
+  const [showReviewScreen, setShowReviewScreen] = useState(false)
   const listOption = [
     'Relevance',
     'Price high to low',
     'Price low to high',
     'Highest number of cleans'
   ]
+  const handleCloseReviewScreen = () => {
+    setShowReviewScreen(false)
+  }
   const updateShortBy = (updateShort) => {
     setupdateShortby(updateShort)
   }
 
   const handleButtonClick = (index, cleaner) => {
-    // Atualiza apenas o CardCleaner clicado
     setSelectedCleaner(index === selectedCleaner ? null : index)
     props.onCleanerSelect(index === selectedCleaner ? null : cleaner)
   }
   const handleButtonReviews = (index) => {
     setSelectedCardIndex(index === selectedCardIndex ? null : index)
   }
-
   
   return (
-    <ContListCleaners style={selectedCardIndex != null ? { opacity: 0.5 } : { opacity: 1 }}>
+    <ContListCleaners >
       <FilterSortby showOption={showOption} onClick={() => setshowOption(!showOption)}>
         <Sortby>Sort by:</Sortby>
         <SortSub>{updateShortby || 'Relevance'}</SortSub>
@@ -271,8 +268,17 @@ export default function ListCleaners(props) {
             key={index}
           >
             {selectedCardIndex === index && (
-              <ReviewScreen onClose={() => handleButtonReviews(index)} />
+              <ReviewScreen
+                cleaner={index === selectedCleaner ? true : false}
+                onClose={() => handleButtonReviews(index)}
+                onButtonClick={() => {
+                  handleButtonClick(index, cleaner)
+                  handleCloseReviewScreen()
+                }}
+                onCleanerinfos={cleanersData[index]}
+              />
             )}
+
             <NameandPric>
               <div style={{ gap: '3px', display: 'flex' }}>
                 <MaleIcon src="/maleicon.png" />
@@ -307,7 +313,9 @@ export default function ListCleaners(props) {
                   color: '#999999',
                   border: '1PX solid #5e5ef5af'
                 }}
-                onClick={() => handleButtonReviews(index)}
+                onClick={() => {
+                  handleButtonReviews(index)
+                }}
               />
 
               <ButtonAltStyled
--- src/components/listcleaners/ReviewScreen.jsx
+++ src/components/listcleaners/ReviewScreen.jsx
@@ -2,6 +2,7 @@ import styled, { keyframes } from 'styled-components'
 import Button from '../form/Button'
 import dynamic from 'next/dynamic'
 import { useState } from 'react'
+import { cleanersData } from './ListCleaners'
 
 const DescAnimation = keyframes`
   from {
@@ -14,40 +15,47 @@ const DescAnimation = keyframes`
 
 const Container = styled.div`
   display: flex;
-  position: fixed;
-  top: 25%;
+  position: absolute;
+  top: 15%;
   left: 25%;
   width: 50vw;
   min-height: 65vh;
   height: auto;
   z-index: 100;
+  @media (max-width: 840px){
+    left: 15%;
+  }
+  @media (max-width: 712px){
+    bottom: 0;
+    top: 100%;
+    height: 90vh
+  }
+  @media (max-width: 712px){
+    left: 7%;
+  } 
+  @media (max-width: 561px){
+    width: 50vh;
+    left: 17%;
+    flex-direction: column;
+  }
+  @media (max-width: 375px){
+    left: 14%;
+  }
 `
 
 const DivCleanerInfos = styled.div`
-  padding: 10px 0 0 10px;
-  width: 50%;
-  height: 100%;
-  background-color: #575757;
-`
-
-const DivReviews = styled.div`
-  background-color: #525252;
-  width: 50%;
-  height: 100%;
-`
-
-const DivInfos = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 10px 0 10px 0;
-  /* código aqui */
+  background: #ffffff;
 `
 
 const DivReviews = styled.div`
   background: #f3f3f3;
   padding: 7px;
+  min-width: 350px;
+  @media (max-width: 561px){
+    min-width: 50vh;
+  }
 `
 const CleanerImg = styled.div`
   width: 50px;
@@ -56,122 +64,86 @@ const CleanerImg = styled.div`
   background-repeat: no-repeat;
   background-image: ${(props) => `url(${props.image})`};
   border-radius: 25%;
+  position: absolute;
+  transform: translate(1%, -50%);
   background-position: center;
+  border: 2px solid #5050eb;
+  border-radius: 25px;
 `
-
 const CleanerName = styled.h2`
-  color: #ccc;
+  color: rgb(68, 68, 68);
+  padding-top: 35px;
 `
-
 const CleanerPrice = styled.h2`
-  color: ${(props) => props.theme.colors.greenMoney};
+  color: #4f538d;
 `
-
 const DivDocInfos = styled.div`
   display: flex;
-  flex-wrap: wrap;
   align-items: center;
-  margin-top: 5px;
-  /* estilos aqui */
+  margin-left: 30px;
+  margin-top: 2px;
 `
-
 const CheckInfo = styled.img`
   width: 15px;
   height: 15px;
 `
-
-const CleaningTypes = styled.div`
-  /* Estilos aqui */
-`
-const DivTypeCleanings = styled.div`
+const TypeCleanings = styled.div`
   display: flex;
   align-items: center;
+  margin-left: 20px;
+  padding: 2px;
 `
-
 const Desc = styled.h2`
   margin: 10px 5px;
-  //color: #77d2df;
-  /* background-image: linear-gradient(
-    45deg,
-    ${(props) => props.theme.colors.greenMoney},
-    #77d2df 50%
-  ); */
-  animation: ${DescAnimation} 4s ease-in-out infinite alternate;
-  //animation-duration: 4s;
-  //animation-iteration-count: infinite;
-  //animation-direction: alternate;
-  /* background-clip: text;
-  -webkit-background-clip: text;
-  -moz-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  -moz-text-fill-color: transparent; */
+  color: rgb(68, 68, 68);
+  //animation: ${DescAnimation} 4s ease-in-out infinite alternate;
 `
-
-const DivBoxAboutMe = styled.div`
-  display: flex;
-  justify-content: center;
-`
-
-const DivBtnRemove = styled.div`
-  display: flex;
-  justify-content: center;
-`
-
 const BoxAboutMe = styled.div`
-  margin: 10px 0 0 0; //C-D-B-E
-  border-radius: 5px;
-  width: fit-content;
-  height: fit-content;
+  padding: 10px;
+  border-radius: 2px;
   word-wrap: break-word;
   word-break: break-all;
-  padding-bottom: 1%;
-  padding-right: 2%;
-  background-color: #32323299;
-  /* ESTILOS AQUI */
+  background-color: #fbfbfb;
 `
 const AboutMeTitle = styled.h2`
-  color: white;
+  color: #252525;
   font-weight: 700;
-  padding: 0 0 0 10px;
+  padding: 0 0 7px 0;
 `
-
-const AboutMeDescDiv = styled.div`
-  /* ESTILOS AQUI */
+const AboutMeSub = styled(AboutMeTitle)`
+  font-weight: 500;
+  font-size: 16px;
 `
 const AboutMeDesc = styled.h3`
-  padding: 10px 0 0 20px;
-  color: white;
+  color: #c8c8c8df;
+  font-weight: 500;
+  font-size: 14px;
+  padding-top: 6px;
 `
-
-const ReviewsDiv = styled.div`
-  color: black;
-  margin: 10px 0 0 10px;
+const BoxReviews = styled.div`
+  margin: 10px;
+  max-height: ${(props) => props.maxHeight || '70vh'};
+  overflow-y: scroll;
 `
-
-const ReviewsDivTitle = styled.div`
-  /* ESTILOS AQUI */
-`
-
 const ReviewsTitle = styled.h2`
-  text-align: center;
-  font-weight: 700;
-  color: ${(props) => props.theme.colors.azulclaro};
+  font-weight: 500;
+  font-size: 17px;
+  color: #252525;
 `
-const ReviewCardDiv = styled.div`
-  color: white;
-`
-
-const ReviewCard = styled.div`
-  font-weight: 700;
-  color: white;
-`
-const ClientReviewName = styled.h3`
-  margin: 10px 0 0 5px;
-  color: white;
+const ClientReviewName = styled.h2`
+  font-size: 14px;
+  color: #212121;
 `
 const ClientReviewDesc = styled.h2`
   font-style: italic;
-  margin: 5px 0 0 20px;
+  font-weight: 500;
+  font-size: 15px;
+  padding-top: 14px;
+  padding-bottom: 15px;
+  line-height: 1.5em;
+  text-align: left;
+  max-width: 500px;
+  height: auto;
 `
 const ClientReviewStars = styled.img`
   background-image: ${(props) => `url(${props.image})`};
@@ -179,13 +151,7 @@ const ClientReviewStars = styled.img`
 `
 const ClientReviewDate = styled.p`
   font-size: 1.3rem;
-  color: white;
-`
-const DivNameDesc = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 30px;
-  margin-top: 10px;
+  color: #373737;
 `
 const DivStarDate = styled.div`
   display: flex;
@@ -211,25 +177,103 @@ const Close = styled.img`
   border-radius: 10px;
   cursor: pointer;
 `
-function ReviewScreen(props) {
+const StarRating = styled.img`
+  width: 17px;
+  height: 17px;
+`
+const RatingAndExpirence = styled.div`
+  background-color: rgb(243, 243, 243);
+  padding: 4px;
+  border-radius: 6px;
+  font-size: 14px;
+  gap: 4px;
+  display: flex;
+  align-items: center;
+`
+const ButtonAlt = styled(Button)`
+  padding: 10px;
+  border-radius: 6px;
+  margin: 20px 20px 70px 20px;
+  background: transparent;
+  border: 1px solid #4141e0;
+  color: #4141e0c4;
+  font-size: 15px;
+  font-weight: 500;
+  :hover {
+    background: #0d0d0dd6;
+    color: white;
+  }
+`
+const ContInfos = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+  margin-bottom: 15px;
+`
+const Rating = styled.img`
+  height: 17px;
+  width: 18px;
+`
+const FlexClientReviewName = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 15px;
+`
+const ContReviewsClient = styled.div`
+  padding: 0 8px 0 0;
+`
+const reviews = [
+  {
+    name: 'Ryan Lucas',
+    desc: 'Katiane did and excellent job. I highly recommend her',
+    rating: 4.6
+  },
+  {
+    name: 'Gabriel Machado',
+    desc: 'Katiane did and excellent job. I highly recommend her',
+    rating: 4.6
+  },
+  {
+    name: 'Joao Pedro',
+    desc: 'Katiane did and excellent job. I highly recommend her',
+    rating: 4.6
+  },
+  {
+    name: 'Ronaldin',
+    desc: 'Katiane did and excellent job. I highly recommend her',
+    rating: 4.6
+  }
+]
+function ReviewScreen({ name, cleaner, ...props }) {
   const [styleQuit, setStyleQuit] = useState(false)
 
   const handleClick = () => {
     setStyleQuit(!styleQuit)
     props.onClose()
   }
+  const handleClickButton = () => {
+    props.onButtonClick()
+    props.onClose()
+    setStyleQuit(!styleQuit)
+  }
   return (
-    <Container style={{ ...(styleQuit === true && { display: 'none' }) }}>
+    <Container {...props} style={{ ...(styleQuit === true && { display: 'none' }) }}>
+      <Close onClick={handleClick} src="/iconcloseW.png" />
       <DivCleanerInfos>
-        <DivInfos>
-          <Quit onClick={handleClick}>QUIT</Quit>
-          <CleanerImg image="/maleicon.png"></CleanerImg>
-          <CleanerName>{props.name}</CleanerName>
-          <CleanerPrice>{props.price}</CleanerPrice>
-        </DivInfos>
-        <div>
-          <hr style={{ marginTop: '15px' }}></hr>
-        </div>
+        <ContInfos>
+          <CleanerImg image="/maleicon.png" /* {props.onCleanerinfos.img} */></CleanerImg>
+          <CleanerName>{props.onCleanerinfos.name}</CleanerName>
+          <CleanerPrice>{props.onCleanerinfos.price}</CleanerPrice>
+          <div style={{ display: 'flex', gap: '4px' }}>
+            <RatingAndExpirence>
+              <StarRating src="/star.png" />
+              {props.onCleanerinfos.rating}
+            </RatingAndExpirence>
+            <RatingAndExpirence>{props.onCleanerinfos.cleaningCount}</RatingAndExpirence>
+          </div>
+        </ContInfos>
+        <hr />
         <DivDocInfos>
           <CheckInfo src="../checkIconB.png" />
           <Desc>Id checks out</Desc>
@@ -238,66 +282,51 @@ function ReviewScreen(props) {
           <CheckInfo src="../checkIconB.png" />
           <Desc>Id checks out</Desc>
         </DivDocInfos>
+        <hr />
+        <TypeCleanings>
+          <ThumbsUp />
+          <Desc>Wet manual cleaning</Desc>
+        </TypeCleanings>
+        <TypeCleanings>
+          <ThumbsUp />
+          <Desc>Dry Cleaning</Desc>
+        </TypeCleanings>
+        <TypeCleanings>
+          <ThumbsUp />
+          <Desc>Dry Cleaning</Desc>
+        </TypeCleanings>
         <hr></hr>
-        <CleaningTypes>
-          <DivTypeCleanings>
-            <ThumbsUp />
-            <Desc>(ThumbsUp image) tipo de limpeza</Desc>
-          </DivTypeCleanings>
-          <DivTypeCleanings>
-            <ThumbsUp />
-            <Desc>(ThumbsUp image) tipo de limpeza</Desc>
-          </DivTypeCleanings>
-          <DivTypeCleanings>
-            <ThumbsUp />
-            <Desc>(ThumbsUp image) tipo de limpeza1</Desc>
-          </DivTypeCleanings>
-        </CleaningTypes>
-        <hr></hr>
-        <DivBtnRemove>
-          <Button
-            style={{ padding: '5px 5px 5px 5px', borderRadius: '6px', marginTop: '5px' }}
-            valor="Remove This Cleaner"
-          />
-        </DivBtnRemove>
+        <ButtonAlt
+          valor={cleaner ? 'Remove this cleaner' : 'Selected this cleaner'}
+          onClick={handleClickButton}
+        />
       </DivCleanerInfos>
-      <hr style={{ width: '5px' }} />
       <DivReviews>
-        <DivBoxAboutMe>
-          <BoxAboutMe>
-            <AboutMeTitle>About Me: </AboutMeTitle>
-            <AboutMeDescDiv>
-              <AboutMeDesc>TEXTO AQUI</AboutMeDesc>
-            </AboutMeDescDiv>
-          </BoxAboutMe>
-        </DivBoxAboutMe>
-
-        <ReviewsDiv>
-          <ReviewsDivTitle>
-            <ReviewsTitle>REVIEWS</ReviewsTitle>
-            <ReviewCardDiv>
-              <ReviewCard>
-                <DivNameDesc>
-                  <hr></hr>
-                  <ClientReviewName>NOME</ClientReviewName>
-                  <ClientReviewDesc>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quam recusandae
-                    ratione, porro dignissimos maiores deserunt voluptas, fuga molestias rem
-                    asperiores suscipit sit? Possimus, deleniti voluptatum reiciendis dolor ratione
-                    maxime? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, sit
-                    omnis! Ex consequuntur quis perferendis dolorem ad velit numquam nemo deleniti
-                    nisi deserunt dignissimos, nulla pariatur tenetur architecto explicabo animi.
-                  </ClientReviewDesc>
-                  <DivStarDate>
-                    <ClientReviewDate>26/07/2002</ClientReviewDate>
-                    <ClientReviewStars src="/maleicon.png" />
-                  </DivStarDate>
-                  <hr></hr>
-                </DivNameDesc>
-              </ReviewCard>
-            </ReviewCardDiv>
-          </ReviewsDivTitle>
-        </ReviewsDiv>
+        <BoxAboutMe>
+          <AboutMeTitle>About Me: </AboutMeTitle>
+          <AboutMeSub>Dedicated to my work</AboutMeSub>
+          <AboutMeDesc>
+            Dedicated to my work. Customer satisfaction is my priority Dedicated to my work.
+            Customer satisfaction is my priority. Good relationship with customer.
+          </AboutMeDesc>
+        </BoxAboutMe>
+        <BoxReviews maxHeight={`${reviews.length * 80}px`}>
+          <ReviewsTitle>Reviews</ReviewsTitle>
+          {reviews.map((review, index) => (
+            <ContReviewsClient key={index}>
+              <FlexClientReviewName>
+                <ClientReviewName>{review.name}</ClientReviewName>
+                <Rating src="/star.png" />
+              </FlexClientReviewName>
+              <ClientReviewDesc>{review.desc}</ClientReviewDesc>
+              <DivStarDate>
+                <ClientReviewDate>26/07/2002</ClientReviewDate>
+                <ClientReviewStars src="/maleicon.png" />
+              </DivStarDate>
+              <hr />
+            </ContReviewsClient>
+          ))}
+        </BoxReviews>
       </DivReviews>
     </Container>
   )
