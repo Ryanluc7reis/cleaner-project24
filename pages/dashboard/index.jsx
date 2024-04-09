4903aa71c66e6909f94c919070af09b31cfb489f
ryanluc7reis <ryan.dev18@outlook.com>
2024-04-09 04:14:24 -0300

    Alterando nome de arquivo  + TODO

---
+++ pages/dashboard/index.jsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+import NavBarDashboard from '../../src/components/layout/NavBarDashboard'
+import Services from '../../src/components/layout/Services'
+import NavRoutesDash from '../../src/components/layout/Navroutesdash'
+import { useState } from 'react'
+
+const Container = styled.div`
+  width: 100%;
+  height: auto;
+`
+
+const StyledFlex = styled.div`
+  display: flex;
+`
+const StyledFlexServices = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #eaeaea;
+  width: 100%;
+`
+const NavBarDashboardAlt = styled(NavBarDashboard)`
+  @media (max-width: 1306px) {
+    display: ${(props) => (props.show ? 'flex' : 'none')};
+  }
+`
+export default function DashboardPage() {
+  const [showDash, setShowDash] = useState(false)
+
+  const handleDash = () => {
+    setShowDash(!showDash)
+  }
+  return (
+    <Container>
+      <StyledFlex>
+        <NavBarDashboardAlt show={showDash} isDash />
+        <StyledFlexServices>
+          <NavRoutesDash onClickDash={handleDash} dash type2 />
+          <Services />
+        </StyledFlexServices>
+      </StyledFlex>
+    </Container>
+  )
+}
